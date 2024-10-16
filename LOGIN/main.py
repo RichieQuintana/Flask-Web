ab1615a7341e2e2d22219f94c703e6c6a4177ad5
RichieQuintana <richi3quintana@gmail.com>
2024-10-15 20:57:02 -0500

    Deploy web

--- LOGIN/main.py
+++ LOGIN/main.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
+import os
 
 app = Flask(__name__)
 app.secret_key = "clave_secreta"
@@ -139,3 +140,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
